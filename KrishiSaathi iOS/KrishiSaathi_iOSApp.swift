975083c33e4efbe2c6152a12adc4aa0bbc1100fa
Rishabh lalwani <rishabhlalwani1048@gmail.com>
2023-09-25 16:26:01 +0530

    Initial Commit

---
+++ KrishiSaathi iOS/KrishiSaathi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrishiSaathi_iOSApp.swift
+//  KrishiSaathi iOS
+//
+//  Created by Rishabh Lalwani on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct KrishiSaathi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
