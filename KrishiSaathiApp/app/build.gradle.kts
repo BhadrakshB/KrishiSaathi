171e25d2a791821f5d7c90af8794170be0a8945b
yashkys <yashkhaitan02@gmail.com>
2023-09-26 12:30:25 +0530

    Initial project setup

--- KrishiSaathiApp/app/build.gradle.kts
+++ KrishiSaathiApp/app/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
+    kotlin("kapt")
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.kys.krishisaathiapp"
     compileSdk = 33
+
+//    dexOptions {
+//        incremental = true
+//    }
 
     defaultConfig {
         applicationId = "com.kys.krishisaathiapp"
@@ -13,35 +22,122 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String", 
+            "API_KEY", 
+            "\"9b8cb8c7f11c077f8c4e217974d9ee40\""
+        )
+        buildConfigField(
+            "String", 
+            "BASE_URL", 
+            "\"https://www.url.com/\""
+        )
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = false
+//            isShrinkResources = true
+            isDebuggable = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+//            signingConfig =  signingConfigs.deb
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+        dataBinding = true
+        viewBinding = true
     }
 }
 
+
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //Android X Core
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.annotation:annotation:1.6.0")
+
+    //Android X Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+
+    //Android X Lifecycle
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+
+    //Android X UI
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    //Material
+    implementation("com.google.android.material:material:1.9.0")
+
+    //Network
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.github.chuckerteam.chucker:library:3.5.2")
+
+    //DI
+    implementation("com.google.dagger:hilt-android:2.46.1")
+    implementation("com.google.dagger:dagger-android-support:2.46.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    //Test
+    implementation("androidx.test:monitor:1.6.1")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+
+    //Lottie
+    implementation("com.airbnb.android:lottie:6.0.1")
+
+    //Image View
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("io.coil-kt:coil:2.4.0")
+
+    //Circular Image View
+    implementation("com.mikhaellopez:circularimageview:4.3.1")
+
+    //firebase
+    implementation( platform("com.google.firebase:firebase-bom:32.1.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation("com.google.firebase:firebase-firestore:24.8.1")
+
+    implementation("com.github.sparrow007:carouselrecyclerview:1.2.6")
+    implementation("com.airbnb.android:lottie:6.0.1")
+    implementation("com.google.firebase:firebase-appcheck-debug:17.0.1")
+
+
+}
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
---
+++ KrishiSaathiApp/app/src/main/java/com/kys/krishisaathiapp/api/FireBaseStorageApi.kt
@@ -0,0 +1,4 @@
+package com.kys.krishisaathiapp.api
+
+class FireBaseStorageApi {
+}
